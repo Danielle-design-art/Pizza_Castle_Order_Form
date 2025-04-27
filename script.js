f88a92933ad4bcf04e80beacdb7183d6358947ac
Danielle-design-art <daniellemorvant1@gmail.com>
2025-04-27 12:51:20 -0500

    added some script for first options on menu

--- script.js
+++ script.js
@@ -1,2 +1,31 @@
 console.log("Help Wanted ;)")
 
+const checkbox = document.getElementById('side1');
+const checkbox2 = document.getElementById('side2');
+const checkbox3= document.getElementById('side3');
+const checkbox4 = document.getElementById('side4');
+const checkbox5 = document.getElementById('side5');
+const checkbox6 = document.getElementById('side6');
+const button = document.getElementById('sidesButton');
+const textarea = document.getElementById('sideTextArea');
+
+
+
+checkbox.addEventListener( 'check' + 'click', function(event) {
+    event.preventDefault()
+    if (checkbox.checked + button.clicked) {
+        textarea.value === checkbox.value;
+    } 
+});
+
+
+
+
+
+
+
+
+
+
+
+
