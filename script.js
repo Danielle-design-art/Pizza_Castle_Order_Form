7f40eec77bae4a769f3866338ff474159058f609
Danielle-design-art <daniellemorvant1@gmail.com>
2025-05-02 00:28:03 -0500

    trial and errors not working

--- script.js
+++ script.js
@@ -2,15 +2,17 @@ console.log("Help Wanted ;)")
 
 document.addEventListener('DOMContentLoaded',function ()  {
 
-const button = document.getElementById('submitOrder');
+
+    
+const button = document.getElementById('submitOrderButton');
 const form =document.getElementById('myForm');
-const tableBody= document.getElementById('oderTabconst');
+
 
 form.addEventListener('submit', function (e) {
 
 
 const myForm = document.getElementById("myForm").value;
-const table = document.getElementById("orderTabconst").value;
+const table = document.getElementById("orderTablet").value;
 const fname1  = document.getElementById("fname").value;
 const pnumber1 = document.getElementById("pnumber").value;
 const language1 = document.getElementById("language");
@@ -26,14 +28,27 @@ const pizza6 = document.createElement("Breakfast Pizza").value;
 
 
 
-const newRow = document.createElement('tr');
-newRow.innerHTML = 
-<td>${custName}</td>;
-<td>specialInstructions</td>;
-<td>order</td>;
-<td>contactNumber</td>;
-<td>orderPreferrence</td>
+function addRow(DOMContentLoaded) {
+  const tableBody = document.getElementById('orderTablet');
+  const newRow = table.insertRow(0);
+  console.log('DOMContentLoaded', "newRow");
+ 
+  const cell1 = newRow.insertCell(1);
+  cell1.textContent = 'New Row, Cell 1';
 
+  const cell2 = newRow.insertCell(2);
+  cell2.textContent = 'New Row, Cell 2';
+
+  const checkboxCell = row.insertCell(3);
+  cell3.checkboxCell = 'New Row, Cell 3';
+
+  const cell4 = newRow.insertCell(4);
+  cell4.textContent = 'New Row, Cell 4';
+
+
+
+ 
+}
 
 tableBody.append(newRow);
 
